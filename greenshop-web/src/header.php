a1474b2b80278efe9855b2c742349ced1fafb7ef
tviolette <tviolette@Ubuntu205.myguest.virtualbox.org>
2025-08-17 15:01:27 +0200

    retest php2

--- greenshop-web/src/header.php
+++ greenshop-web/src/header.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <header>
-        <h1><a href="index.php">greenShop powered by Jenkins ! =]</a></h1>
+        <h1><a href="index.php">greenShop powered by Jenkins et un peu de chance! =]</a></h1>
         <nav>
             <a href="index.php">Home</a>
             <a href="cart.php">Cart</a>
